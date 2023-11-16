c724b118e760e48c30a366a4787f471a44f6500d
Vasilii Nalimov <nva070991@gmail.com>
2023-11-16 16:26:37 +0700

    1611

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import Button from '../../components/Button/Button';
 import Heading from '../../components/Heading/Heading';
 import Input from '../../components/Input/Input';
 import styles from './Login.module.css';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 
-
-export type LoginForm = {
+type LoginForm = {
 	loginEmail: {
 		value: string;
 	};
@@ -16,33 +14,41 @@ export type LoginForm = {
 };
 
 export function Login() {
+	const [loginErrorMessage, setLoginErrorMessage] = useState('');
+
 	const submit = async (e: FormEvent) => {
 		e.preventDefault();
+
 		const target = e.target as typeof e.target & LoginForm;
 		const { loginEmail, loginPassword } = target;
+
+		if (!loginEmail.value || !loginPassword.value) {
+			setLoginErrorMessage('Введите email и пароль');
+			return;
+		}
+
 		await checkData(loginEmail.value, loginPassword.value);
 	};
 
-	
-
 	const checkData = async (loginEmail: string, loginPassword: string) => {
 		try {
-			const response = await fetch(
-				'https://purpleschool.ru/pizza-api-demo/auth/login',
-				{
-					method: 'POST',
-					body: JSON.stringify({ email: loginEmail, password: loginPassword })
-				}
-			);
+			const response = await fetch('', {
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify({ email: loginEmail, password: loginPassword })
+			});
 			if (!response.ok) {
-				console.log('Запрос завершился с ошибкой', response.status);
+				setLoginErrorMessage('Неверная почта или пароль');
 				return;
 			}
+			setLoginErrorMessage('');
 			const data = await response.json();
-			console.log('Данные получены', data);
+			console.log('Промис с токеном', data);
 		} catch (e) {
 			console.error(e);
-			return;
 		}
 	};
 
@@ -50,6 +56,10 @@ export function Login() {
 		<>
 			<div className={styles['login']}>
 				<Heading>Вход</Heading>
+				{loginErrorMessage && (
+					<div className={styles['error']}>Error: {loginErrorMessage}</div>
+				)}
+
 				<form className={styles['form']} onSubmit={submit}>
 					<div className={styles['field']}>
 						<label htmlFor="loginEmail">Ваш e-mail</label>
